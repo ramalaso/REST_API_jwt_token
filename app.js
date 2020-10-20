a20cf30ffa65e9a92c1deeccb076a535170bf658
Raul Laredo Soria <Raul.Laredo@jalasoft.com>
2020-10-20 14:27:23 -0400

    Added jwt

--- app.js
+++ app.js
@@ -9,9 +9,16 @@ app.get('/api', (req, res) => {
     })
 });
 
-app.post('/api/posts', (req, res)=>{
-    res.json({
-        message: 'Post created...'
+app.post('/api/posts', verifyToken, (req, res)=>{
+    jwt.verify(req.token, 'secretkey', (err, authData)=>{
+        if(err) {
+            res.sendStatus(403)
+        } else {
+            res.json({
+                message: 'Post created...',
+                authData
+            })
+        }
     })
 })
 
@@ -29,6 +36,23 @@ app.post('/api/login', (req, res)=>{
         })
     })
 })
+
+function verifyToken(req, res, next){
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    //Check if bearer is not undefined
+    if(typeof bearerHeader !== 'undefined') {
+        //Split at the space
+        const bearer = bearerHeader.split(' ')
+        //Get token from array
+        const bearerToken = bearer[1]
+        req.token = bearerToken
+        //Next middleware
+        next()
+    } else {
+        res.sendStatus(403)
+    }
+}
 
 app.listen(3000, () => {
     console.log('App listening on port 3000!');
